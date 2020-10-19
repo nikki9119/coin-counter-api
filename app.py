1af80f5eff2f0af918ef524b0002023d0259b431
nikki9119 <a.p.nikhilkumar@gmail.com>
2020-10-19 19:23:42 +0530

    app changes

--- app.py
+++ app.py
@@ -2,37 +2,25 @@ import keras_preprocessing
 from keras_preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array
 from tensorflow.keras.models import load_model, model_from_json
-# from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 
 import os
-# from zipfile import ZipFile
 import numpy as np
+import base64
 
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# MODEL_PATH = 'rps.h5'
-
-# model = load_model('rps.h5')
-with open('model/model_config.json') as json_file:
-    json_config = json_file.read()
-    model = model_from_json(json_config)
-model.load_weights('model/rps_weights.h5')
-# model._make_predict_function()
+model = load_model('rps.h5')
+print('Model loaded')
 
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(300, 300))
 
-    # Preprocessing the image
     x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
     images = np.vstack([x])
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    #x = preprocess_input(x, mode='caffe')
 
     preds = model.predict(images,batch_size=10)
     return preds
@@ -41,32 +29,47 @@ def model_predict(img_path, model):
 def hello():
     return jsonify({"about":"Hello World"})
 
-# @app.route("/get_name")
-# def helloname():
-#     return jsonify({"about":"Hello World, I am Nikhil"})
+@app.route("/get_name")
+def helloname():
+    return jsonify({"about":"Hello World, I am Nikhil"})
+
+@app.route("/json_req",methods=['GET','POST'])
+def resendreq():
+    req_str = request.get_json()
+    strng = req_str['str']
+    return jsonify({'str':strng})
 
 @app.route("/predict",methods=['GET','POST'])
 def upload():
+    print('Inside Upload')
     if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+        print('Inside Post')
 
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+        f = request.get_json()
+        imgstring = f['image']
+        imgdata = base64.b64decode(imgstring)
+        filename = 'some_image.jpg'
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
 
-        # Make prediction
-        preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        # result = str(pred_class[0][0][1])               # Convert to string
-        return preds
+        preds = model_predict(filename, model)
+        print(preds)
+        max = 0.0
+        if(preds[0][0] and preds[0][0] > max):
+            max = preds[0][0]
+            value = 1
+        if(preds[0][1] and preds[0][1] > max):
+            max = preds[0][1]
+            value = 10
+        if(preds[0][2] and preds[0][2] > max):
+            max = preds[0][2]
+            value = 2
+        if(preds[0][3] and preds[0][3] > max):
+            max = preds[0][3]
+            value = 5
+        return jsonify({"preds":value})
     return None
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,host='0.0.0.0')
